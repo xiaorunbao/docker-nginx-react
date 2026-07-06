0ef00badffdb468445c7ff77b1080d7df0682af0
rainbow <5429529781@qq.com>
2026-07-06 10:06:15 +0800

    feat: remove api-geteway

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,11 @@
 FROM nginx:alpine
-LABEL maintainer="zzswang@gmail.com"
+LABEL maintainer="rainbow@gmail.com"
 
 ENV DEBUG=off \
   BUILD="1997-01-01T00:00:00.000Z" \
   APP_VERSION="v0.0.0" \
   APP_WORKDIR=/app \
-  APP_BASENAME=/aSubSiteInParentDomainUseThisPath \
-  API_PLACEHOLDER=/allRequestStartOfthisPathIsAnApiCall \
-  API_GATEWAY="https://api.adventurer.tech" \
-  API_PLACEHOLDER_1=/allRequestStartOfthisPathIsAnApiCall_1 \
-  API_GATEWAY_1="https://api.adventurer.tech" \
-  API_PLACEHOLDER_2=/allRequestStartOfthisPathIsAnApiCall_2 \
-  API_GATEWAY_2="https://api.adventurer.tech" \
-  API_PLACEHOLDER_3=/allRequestStartOfthisPathIsAnApiCall_3 \
-  API_GATEWAY_3="https://api.adventurer.tech" \
-  API_PLACEHOLDER_4=/allRequestStartOfthisPathIsAnApiCall_4 \
-  API_GATEWAY_4="https://api.adventurer.tech" \
-  API_PLACEHOLDER_5=/allRequestStartOfthisPathIsAnApiCall_5 \
-  API_GATEWAY_5="https://api.adventurer.tech" \
+  APP_BASENAME=/aSubSite \
   CLIENT_BODY_TIMEOUT=10 \
   CLIENT_HEADER_TIMEOUT=10 \
   CLIENT_MAX_BODY_SIZE=1024 \
--- start-nginx.sh
+++ start-nginx.sh
@@ -1,58 +1,53 @@
 #!/bin/sh
 
 echo "setting nginx conf ..."
-echo "DEBUG": $DEBUG
-echo "BUILD": $BUILD
-echo "ENV_PREFIX": $ENV_PREFIX
-echo "WHITE_LIST": $WHITE_LIST
-echo "WHITE_LIST_IP": $WHITE_LIST_IP
-echo "APP_VERSION": $APP_VERSION
-echo "APP_WORKDIR": $APP_WORKDIR
-echo "APP_BASENAME": $APP_BASENAME
-echo "API_PLACEHOLDER": $API_PLACEHOLDER
-echo "API_PLACEHOLDER_1": $API_PLACEHOLDER_1
-echo "API_PLACEHOLDER_2": $API_PLACEHOLDER_2
-echo "API_PLACEHOLDER_3": $API_PLACEHOLDER_3
-echo "API_PLACEHOLDER_4": $API_PLACEHOLDER_4
-echo "API_PLACEHOLDER_5": $API_PLACEHOLDER_5
-echo "API_GATEWAY": $API_GATEWAY
-echo "API_GATEWAY_1": $API_GATEWAY_1
-echo "API_GATEWAY_2": $API_GATEWAY_2
-echo "API_GATEWAY_3": $API_GATEWAY_3
-echo "API_GATEWAY_4": $API_GATEWAY_4
-echo "API_GATEWAY_5": $API_GATEWAY_5
-echo "CLIENT_BODY_TIMEOUT": $CLIENT_BODY_TIMEOUT
-echo "CLIENT_HEADER_TIMEOUT": $CLIENT_HEADER_TIMEOUT
-echo "CLIENT_MAX_BODY_SIZE": $CLIENT_MAX_BODY_SIZE
+echo "DEBUG: $DEBUG"
+echo "BUILD: $BUILD"
+echo "ENV_PREFIX: $ENV_PREFIX"
+echo "WHITE_LIST: ${WHITE_LIST}"
+echo "WHITE_LIST_IP: ${WHITE_LIST_IP}"
+echo "APP_VERSION: ${APP_VERSION}"
+echo "APP_WORKDIR: ${APP_WORKDIR}"
+echo "APP_BASENAME: ${APP_BASENAME}"
+echo "CLIENT_BODY_TIMEOUT: ${CLIENT_BODY_TIMEOUT}"
+echo "CLIENT_HEADER_TIMEOUT: ${CLIENT_HEADER_TIMEOUT}"
+echo "CLIENT_MAX_BODY_SIZE: ${CLIENT_MAX_BODY_SIZE}"
 
-## Replace env for nginx conf
-envsubst '$DEBUG $BUILD $WHITE_LIST $WHITE_LIST_IP $APP_VERSION $APP_WORKDIR $APP_BASENAME $API_PLACEHOLDER $API_GATEWAY $API_PLACEHOLDER_1 $API_GATEWAY_1 $API_PLACEHOLDER_2 $API_GATEWAY_2 $API_PLACEHOLDER_3 $API_GATEWAY_3 $API_PLACEHOLDER_4 $API_GATEWAY_4 $API_PLACEHOLDER_5 $API_GATEWAY_5 $CLIENT_BODY_TIMEOUT $CLIENT_HEADER_TIMEOUT $CLIENT_MAX_BODY_SIZE' < /etc/nginx/conf.d/app.conf.template > /etc/nginx/conf.d/default.conf
+# Replace env for nginx conf
+envsubst \
+  '$DEBUG $BUILD $WHITE_LIST $WHITE_LIST_IP $APP_VERSION $APP_WORKDIR \
+$APP_BASENAME $CLIENT_BODY_TIMEOUT $CLIENT_HEADER_TIMEOUT $CLIENT_MAX_BODY_SIZE' \
+  < /etc/nginx/conf.d/app.conf.template > /etc/nginx/conf.d/default.conf
 
-## Delete white list config if white list feature off 
-if [ ${WHITE_LIST} = 'off' ]; then
-    # delete white list config
-    sed -i '/^[ ]*\#[ ]*BEGIN_CONFIG_WHEN_WHITE_LIST_ON/,/^[ ]*\#[ ]*END_CONFIG_WHEN_WHITE_LIST_ON/{d;};' /etc/nginx/conf.d/default.conf
+# Delete white list config if white list feature is off
+if [ "${WHITE_LIST}" = 'off' ]; then
+  sed -i '/^[[:space:]]*#[[:space:]]*BEGIN_CONFIG_WHEN_WHITE_LIST_ON/,/^[[:space:]]*#[[:space:]]*END_CONFIG_WHEN_WHITE_LIST_ON/d' /etc/nginx/conf.d/default.conf
 fi
 
-## inject runtime env into *.html
-ENV_SUBS=$(echo $(env | cut -d= -f1 | grep "^${ENV_PREFIX}" | sed -e 's/^/\$/'))
+# Inject runtime env into *.html files
+ENV_SUBS=$(env | cut -d= -f1 | grep "^${ENV_PREFIX}" | sed -e 's/^/\$/')
 echo "inject runtime environments ..."
-### Recreate env-runtime file
+
+# Recreate env-runtime file
 rm -rf ./env-runtime.js
 touch ./env-runtime.js
-### Add assignment
+
+# Build runtime environment JavaScript object
 echo "window._runtime_ = {" >> ./env-runtime.js
 for e in $ENV_SUBS; do
-  # Append configuration property to JS file
-  eName=$(echo $e | sed -e 's/^\$//');
-  value=$(echo $(eval echo "\$$eName"));
-  echo "  $eName: \"$value\"," >> ./env-runtime.js
+  eName=$(echo "$e" | sed -e 's/^\$//')
+  value=$(eval echo "\"\$${eName}\"")
+  printf '  %s: "%s",\n' "$eName" "$value" >> ./env-runtime.js
 done
 echo "}" >> ./env-runtime.js
-sed -i -e 's/<script src="\/env-runtime.js"><\/script>//g' *.html
-sed -i -e 's/<body>/&<script src="\/env-runtime.js"><\/script>/' *.html
 
-## Start nginx
+# Inject env-runtime.js script tag into HTML files
+sed -i \
+  -e 's/<script src="\/env-runtime.js"><\/script>//g' \
+  -e 's/<body>/&<script src="\/env-runtime.js"><\/script>/' \
+  *.html
+
+# Start nginx
 echo "start nginx"
 nginx -g 'daemon off;'
 exec "$@"
